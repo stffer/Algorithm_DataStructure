ea96e50881b395d09046fd58f5fe6c3f379663b7
stffer <771192663@qq.com>
2017-02-25 13:12:59 +0800

    quick_sort

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,6 +1,6 @@
 a=[9,1,2,6,5,123,123,45,2,13,567,213,1,223,2,-12,1222,34,11];
-#a[30]={2,1};
 def quick_sort(l,r):
+
     if l>=r:return
     key_value = a[l]
     left,right=l,r
@@ -14,5 +14,6 @@ def quick_sort(l,r):
     a[left]=key_value
     quick_sort(l,left-1)
     quick_sort(left+1,r)
+
 quick_sort(0,len(a)-1)
 print(a)
